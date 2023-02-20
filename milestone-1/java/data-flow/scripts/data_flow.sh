2af069b2d7327f2f85123e40fafd888843b079d2
Kedar <kedar.takwane@gmail.com>
2023-02-20 01:52:37 -0600

    Added data-flow graphs for Java files

--- milestone-1/java/data-flow/scripts/data_flow.sh
+++ milestone-1/java/data-flow/scripts/data_flow.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 
 echo 'digraph {
-	start -> a;
-	# while -> a;
-	# while -> end;
-	a -> end
-	# a -> if;
-	# if -> b;
-	# if -> c;
-	# b -> c;
-	# b-> end;
-	# a -> end;
+	start -> if1;
+	if1 -> a;
+	a -> if2;
+	if2 -> b;
+	b -> c;
+	if2 -> c;
+	c -> d;
+	if1 -> d;
 
-	a [shape=box, label="columnMetadataList = getColumns(...)"]	
-	# b [shape=box, label="lastCommitTime = nanoTime(...)"]
-}' | dot -Tsvg > CdcSourcePresolveMappingColumns.svg
+	b [shape=box, label="result = startsWith(...)"]	
+	# b [shape=box, label = "snapshotTraverser = onFirstNull(...)"]
+	# e [shape=box, label = "queries = new Queries(...)"]
+	# f [shape=box, label = "initFailure = e"]
+	# if [shape=diamond, label="if"]
+	# if2 [shape=diamond, label="if2"]
+	# if3 [shape=diamond, label="if3"]
+
+}' | dot -Tsvg > GenericMapStoreisIntegrityConstraintViolation.svg
