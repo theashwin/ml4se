7bb4b22f18fa8bfc4a4326fe90cbccafcf341a9a
Kedar <kedar.takwane@gmail.com>
2023-02-20 03:24:52 -0600

    Added data-flow graphs for python functions

--- milestone-1/java/data-flow/scripts/data_flow.sh
+++ milestone-1/java/data-flow/scripts/data_flow.sh
@@ -1,21 +1,28 @@
 #!/bin/bash
 
 echo 'digraph {
-	start -> if1;
-	if1 -> a;
-	a -> if2;
-	if2 -> b;
+	start -> a;
+	a -> try1;
+	try1 -> b;
 	b -> c;
-	if2 -> c;
-	c -> d;
-	if1 -> d;
+	try1 -> except1;
+	except1 -> d;
+	d -> end;
+	c -> try2;
+	try2 -> e;
+	e -> end;
+	try2 -> except2;
+	except2 -> f;
+	f -> end;
 
-	b [shape=box, label="result = startsWith(...)"]	
-	# b [shape=box, label = "snapshotTraverser = onFirstNull(...)"]
-	# e [shape=box, label = "queries = new Queries(...)"]
-	# f [shape=box, label = "initFailure = e"]
-	# if [shape=diamond, label="if"]
-	# if2 [shape=diamond, label="if2"]
+	a [shape=box, label="makefile = get_makefile_filename(...)"]	
+	b [shape=box, label = "_parse_makefile(makefile, ...)"]
+	c [shape=box, label = "config_h = get_config_h_filename(...)"]
+	d [shape=box, label = "msg = ... % makefile"]
+	e [shape=box, label = "open(config_h)"]
+	f [shape=box, label = "msg = ... % config_h"]
+	# if1 [shape=diamond, label="if"]
+	# else1 [shape=diamond, label="else1"]
 	# if3 [shape=diamond, label="if3"]
 
-}' | dot -Tsvg > GenericMapStoreisIntegrityConstraintViolation.svg
+}' | dot -Tsvg > 71.svg
